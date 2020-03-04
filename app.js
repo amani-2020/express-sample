be3dd6b01f6927bf962ba411383521dc939afe68
Alhaw <1996048@cegep-heritage.qc.ca>
2020-03-04 11:49:35 -0500

    add image folder

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express();
 const port = 9000;
 
 app.get("/", (req, res) => {
-  res.send("a string!");
+  res.send(
+    `<h1> Good Morning!</h1> <p>hello</p><h3>Bye</h3><img src="img/p17.jpg"/>`
+  );
 });
 
 app.listen(port, () => {
