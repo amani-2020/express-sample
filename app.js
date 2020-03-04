943c9001953f09fb8a7838919223e4338ebd5952
Alhaw <1996048@cegep-heritage.qc.ca>
2020-03-04 11:33:27 -0500

    first  response

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const port = 9000;
+
+app.get("/", (req, res) => {
+  res.send("a string!");
+});
+
+app.listen(port, () => {
+  console.log(`listening on port: ${port}`);
+});
