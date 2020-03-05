f2b96933ac5a941245f77a906f1b6bf2ac1a2db2
Alhaw <1996048@cegep-heritage.qc.ca>
2020-03-05 10:32:00 -0500

    add post

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require("express");
-
 const app = express();
-
 const port = 9000;
 
-app.get("/", (req, res) => {
-  res.send(
-    `<h1> Good Morning!</h1> <p>hello</p><h3>Bye</h3><img src="img/p17.jpg"/>`
-  );
+// app.get("/cat+s/", (req, res) => {
+//   res.send(`<h1>Cats!</h1>`);
+// });
+
+// app.get(/(cat|dog)/, (req, res) => {
+//   res.send(`<h1>Cats!</h1>`);
+// });
+
+app.get("/do?gs", (req, res) => {
+  res.send(`<h1>Dogs!</h1>`);
+});
+app.post("/do?gs", (req, res) => {
+  res.send(`<h1>Added a dog!</h1>`);
+});
+
+app.get("/ze(bra)+", (req, res) => {
+  res.send(`<h1>Zebras!</h1>`);
+});
+
+app.get("/pen*guin", (req, res) => {
+  res.send(`<h1>Penguin!</h1>`);
 });
 
 app.listen(port, () => {
